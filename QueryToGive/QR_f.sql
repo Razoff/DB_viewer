b81e162a83adbfcb0f2622851545699060921006
Arnaud Garin <arnaud.garin@epfl.ch>
2016-06-02 13:55:08 +0200

    ?

--- QueryToGive/QR_f.sql
+++ QueryToGive/QR_f.sql
@@ -1,10 +1,8 @@
-SELECT  *
+SELECT tit
 FROM (SELECT tii.T_TITLE as tit, count(tii.T_TITLE) as nbTrad, tii.LANGUAGE_ID as lang
 FROM TITLE ti, TITLE tii
 WHERE ti.PARENT !=0 and ti.PARENT = tii.T_ID and tii.LANGUAGE_ID = ti.LANGUAGE_ID
 group by tii.T_TITLE, tii.LANGUAGE_ID
 order by -count(tii.T_TITLE)) q1
-group by lang
-
-
+WHERE rec = 3
 -- ROW_NUMBER() OVER(PARTITION ORDER BY ... DESC)
