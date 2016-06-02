04b334f78c881597705a5fe0ea38bb969aeb46c3
Arnaud Garin <arnaud.garin@epfl.ch>
2016-06-02 14:27:29 +0200

    SQL te donne des L

--- QueryToGive/QR_k.sql
+++ QueryToGive/QR_k.sql
@@ -1,5 +1,5 @@
-SELECT AVG(q1.nb)
+SELECT ROUND(AVG(q1.nb),2) as nbPub -- average them 
 FROM  (SELECT pub.PUBLICATION_SERIES_ID, count(pub.PUBLICATION_SERIES_ID) as nb
       FROM PUBLICATIONS pub
-      WHERE pub.PUBLICATION_SERIES_ID IS NOT NULL
-      GROUP BY pub.PUBLICATION_SERIES_ID) q1
+      WHERE pub.PUBLICATION_SERIES_ID IS NOT NULL -- different number of pub
+      GROUP BY pub.PUBLICATION_SERIES_ID) q1 -- group them
