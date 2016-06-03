c3a36702be08f4295ed581f6036034d45f30fe78
Arnaud Garin <arnaud.garin@epfl.ch>
2016-06-03 18:54:08 +0200

    Oz

---
+++ QueryS2/QR_g.sql
@@ -0,0 +1,9 @@
+SELECT tit.T_TITLE
+FROM  (SELECT q1.title as theOne, count(q1.rew) + COUNT(q1.awd)
+      FROM  (SELECT DISTINCT rw.TITLE_ID as title , rw.REVIEW_ID as rew, ta.AWARD_ID as awd
+            FROM REVIEW rw , TITLE_AWARD ta
+            WHERE rw.TITLE_ID = ta.TITLE_ID) q1
+      GROUP BY q1.title
+      ORDER BY count(q1.rew) + COUNT(q1.awd) DESC) q2, TITLE tit
+WHERE ROWNUM < 4 and q2.theOne = tit.T_ID
+
