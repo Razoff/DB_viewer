c2ad57d77c6f0a4266c538fc68e1a5ea021c16ae
Arnaud Garin <arnaud.garin@epfl.ch>
2016-06-03 17:23:57 +0200

    It's call garbage can not garbage cannot

--- QueryToGive/QR_h.sql
+++ QueryToGive/QR_h.sql
@@ -1,6 +1,6 @@
 SELECT ps.P_S_NAME
 FROM  (SELECT pub.PUBLICATION_SERIES_ID as pnnb, count(pub.PUBLICATION_SERIES_ID) -- the publication series number of everyone that won the price
-      FROM  (SELECT taw.TITLE_ID as title -- query gives all title that wins this award
+      FROM  (SELECT DISTINCT taw.TITLE_ID as title -- query gives all title that wins this award
             FROM TITLE tit, TITLE_AWARD taw, AWARDS aw, AWARD_TYPES awt
             WHERE tit.T_ID = taw.TITLE_ID and taw.AWARD_ID = aw.AW_ID and aw.AW_TYPE_ID = awt.AW_T_ID and awt.AW_T_NAME = 'World Fantasy Award') q1
       , PUBLICATION_CONTENT pc, PUBLICATIONS pub
