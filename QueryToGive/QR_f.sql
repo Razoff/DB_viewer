92baa83e79c326ad29bf90140fb46b8cfbae7955
Arnaud Garin <arnaud.garin@epfl.ch>
2016-06-03 13:33:17 +0200

    f*** me I m famous

--- QueryToGive/QR_f.sql
+++ QueryToGive/QR_f.sql
@@ -1,8 +1,10 @@
-SELECT tit
-FROM (SELECT tii.T_TITLE as tit, count(tii.T_TITLE) as nbTrad, tii.LANGUAGE_ID as lang
-FROM TITLE ti, TITLE tii
-WHERE ti.PARENT !=0 and ti.PARENT = tii.T_ID and tii.LANGUAGE_ID = ti.LANGUAGE_ID
-group by tii.T_TITLE, tii.LANGUAGE_ID
-order by -count(tii.T_TITLE)) q1
-WHERE rec = 3
--- ROW_NUMBER() OVER(PARTITION ORDER BY ... DESC)
+SELECT *
+FROM (SELECT tii.T_TITLE as tit, count(tii.T_TITLE) as nbTrad, tii.LANGUAGE_ID as lang, 
+      ROW_NUMBER () OVER (PARTITION BY tii.LANGUAGE_ID
+                    ORDER BY count(tii.T_TITLE) DESC) as rec
+      FROM TITLE ti, TITLE tii
+      WHERE ti.PARENT !=0 and ti.PARENT = tii.T_ID and tii.LANGUAGE_ID = ti.LANGUAGE_ID
+      group by tii.T_TITLE, tii.LANGUAGE_ID
+      order by -count(tii.T_TITLE)) q1
+WHERE rec <= 3
+
